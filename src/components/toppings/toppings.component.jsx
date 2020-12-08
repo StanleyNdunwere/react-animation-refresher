b6a05e77dc941fbb725893eac7eb54e7111b87f4
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2020-12-08 15:23:56 +0100

    added state for toppings component

--- src/components/toppings/toppings.component.jsx
+++ src/components/toppings/toppings.component.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonComponent from "../../global/components/button/button.component";
 import imgSrc from "../../assets/images/right-arrow.png";
 import "../toppings/toppings.component.css";
 
 export default function ToppingsComponent(props) {
+  const [toppingValue, setToppingValue] = useState([]);
+
+  let handleClick = (value, toppingValue) => {
+    if (toppingValue.includes(value)) {
+      let newToppingsList = toppingValue.filter((topping) => {
+        return topping !== value;
+      });
+      setToppingValue(newToppingsList);
+    } else {
+      setToppingValue([...toppingValue, value]);
+    }
+  };
+  const toppingsOptions = [
+    "mushrooms",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes",
+  ];
   return (
     <div className="base-content">
       <h3 className="base-title">Step 2: Choose Toppings</h3>
       <hr />
-      <BaseItem content="mushrooms"></BaseItem>
-      <BaseItem content="peppers"></BaseItem>
-      <BaseItem content="onions"></BaseItem>
-      <BaseItem content="olives"></BaseItem>
-      <BaseItem content="extra cheese"></BaseItem>
-      <BaseItem content="tomatoes"></BaseItem>
+      {toppingsOptions.map((topping) => {
+        return (
+          <BaseItem
+            key={topping}
+            optionValue={topping}
+            clickOption={() => handleClick(topping, toppingValue)}
+            toppingsList={toppingValue}
+          />
+        );
+      })}
       <br />
       <ButtonComponent
         {...props}
@@ -26,10 +50,15 @@ export default function ToppingsComponent(props) {
 }
 
 function BaseItem(props) {
+  const { optionValue, toppingsList } = { ...props };
   return (
-    <div className="base-option">
-      <img alt="right arrow button" className="img-icon" src={imgSrc} />
-      <span className="option-name">{props.content}</span>
+    <div className="base-option" onClick={props.clickOption}>
+      {toppingsList.includes(optionValue) ? (
+        <img alt="right arrow button" className="img-icon" src={imgSrc} />
+      ) : (
+        <span></span>
+      )}
+      <span className="option-name">{props.optionValue}</span>
     </div>
   );
 }
